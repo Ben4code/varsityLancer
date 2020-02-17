a5609c924e929fe04b1543ca73185cee8c775ad4
ben4code <nnaemeka.obioha@gmail.com>
2020-02-17 00:27:55 +0000

    Updated nav links

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -126,21 +126,21 @@ export default function Header(props) {
     if (window.location.pathname === '/' && value !== 0) {
       setValue(0)
       setSelectedIndex(0)
-    } else if (window.location.pathname === '/about' && value !== 1) {
+    // } else if (window.location.pathname === '/about' && value !== 1) {
+    //   setValue(1)
+    //   setSelectedIndex(1)
+    } else if (window.location.pathname === '/join' && value !== 1) {
       setValue(1)
       setSelectedIndex(1)
-    } else if (window.location.pathname === '/join' && value !== 2) {
+    } else if (window.location.pathname === '/as-dev' && value !== 1) {
+      setValue(1)
+      setSelectedIndex(1)
+    } else if (window.location.pathname === '/as-client' && value !== 1) {
+      setValue(1)
+      setSelectedIndex(1)
+    } else if (window.location.pathname === '/dashboard' && value !== 2) {
       setValue(2)
       setSelectedIndex(2)
-    } else if (window.location.pathname === '/as-dev' && value !== 2) {
-      setValue(2)
-      setSelectedIndex(2)
-    } else if (window.location.pathname === '/as-client' && value !== 2) {
-      setValue(2)
-      setSelectedIndex(2)
-    } else if (window.location.pathname === '/dashboard' && value !== 3) {
-      setValue(3)
-      setSelectedIndex(3)
     }
   }, [value])
 
@@ -161,14 +161,14 @@ export default function Header(props) {
             onClick={()=> {setOpenDrawer(false); setValue(0)}} selected={value === 0} component={Link} to="/" >
             <ListItemText disableTypography className={value === 0 ? [classes.drawerItemSelected, classes.drawerItem] : classes.drawerItem } >Home</ListItemText>
           </ListItem>
-          <ListItem divider button disableRipple onClick={()=> {setOpenDrawer(false); setValue(1)}} selected={value === 1} component={Link} to="/about" >
+          {/* <ListItem divider button disableRipple onClick={()=> {setOpenDrawer(false); setValue(1)}} selected={value === 1} component={Link} to="/about" >
             <ListItemText disableTypograph className={value === 1 ? [classes.drawerItemSelected, classes.drawerItem] : classes.drawerItem }>About</ListItemText>
+          </ListItem> */}
+          <ListItem divider button disableRipple onClick={()=> {setOpenDrawer(false); setValue(1)}} selected={value === 1} component={Link} to="/join" >
+            <ListItemText disableTypography className={value === 1 ? [classes.drawerItemSelected, classes.drawerItem] : classes.drawerItem }>Join</ListItemText>
           </ListItem>
-          <ListItem divider button disableRipple onClick={()=> {setOpenDrawer(false); setValue(2)}} selected={value === 2} component={Link} to="/join" >
-            <ListItemText disableTypography className={value === 2 ? [classes.drawerItemSelected, classes.drawerItem] : classes.drawerItem }>Join</ListItemText>
-          </ListItem>
-          <ListItem divider button disableRipple onClick={()=> {setOpenDrawer(false); setValue(3)}} selected={value === 3} component={Link} to="/dashboard">
-            <ListItemText disableTypography className={value === 3 ? [classes.drawerItemSelected, classes.drawerItem] : classes.drawerItem }>Dashboard</ListItemText>
+          <ListItem divider button disableRipple onClick={()=> {setOpenDrawer(false); setValue(2)}} selected={value === 2} component={Link} to="/dashboard">
+            <ListItemText disableTypography className={value === 2 ? [classes.drawerItemSelected, classes.drawerItem] : classes.drawerItem }>Dashboard</ListItemText>
           </ListItem>
 
         </List>
@@ -188,13 +188,14 @@ export default function Header(props) {
           aria-haspopup={anchorEl ? "true" : undefined}
           onMouseOver={e => handleClick(e)}
           className={classes.tab} component={Link} to="join" label="Join" />
+
         <Tab className={classes.tab} component={Link} to="dashboard" label="Dashboard" />
       </Tabs>
 
       <Menu id="simple-menu" anchorEl={anchorEl} open={openMenu} onClose={handleClose} MenuListProps={{ onMouseLeave: handleClose }}
         classes={{ paper: classes.menu }} elevation={0}>
         {menuOptions.map((option, i) => (
-          <MenuItem key={option.name} component={Link} to={option.link} classes={{ root: classes.menuItem }} onClick={(e) => { handleMenuItemClick(e, i); setValue(2); handleClose() }} selected={i === selectedIndex}>
+          <MenuItem key={option.name} component={Link} to={option.link} classes={{ root: classes.menuItem }} onClick={(e) => { handleMenuItemClick(e, i); setValue(1); handleClose() }} selected={i === selectedIndex}>
             {option.name}
           </MenuItem>
         ))}
